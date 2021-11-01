f70f3cbfc75e4653d153bef69fbbd90b856cd203
jiaweny <37666860+jiaweny@users.noreply.github.com>
2021-11-01 00:56:47 -0500

    'First commit'

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="snspec",
+    version="0.0.1",
+    author="Jiawen Yang",
+    author_email="jiawen.yang096@gmail.com",
+    description="A package to analyze Type Ia supernova spectra.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yang-jiawen/snspec",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
